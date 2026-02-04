b25472b343b61192bf78c6199c1d4864ab8861fd
VAIBHAV SHARMA <sharmavaibhav447@gmail.com>
2026-02-04 21:43:51 +0530

    CORS Updated

--- backend/server/index.js
+++ backend/server/index.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: [
     "http://localhost:3001",
-    "https://mafia-game-frontend.vercel.app"
+    "https://mafia-game-topaz.vercel.app/"
   ],
     methods: ["GET", "POST"]}
 });
